3a64ec70235a17c88269f9e1c658082033c22e74
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-03-06 19:09:08 +0100

    menu text and better pattern handling

--- src/main.py
+++ src/main.py
@@ -76,13 +76,18 @@ class Cell:
         self.north = True
         self.south = True
 
+        self.pattern = False
+
 
     def render(self, data_addr, line_len, bpp) -> None:
         x = self.row * self.height
         y = self.column * self.width
         wall_size = self.width//20
+        if self.pattern:
+            put_box(data_addr, line_len, bpp, x, y, self.width, self.height, GREEN) # inner
+            return
 
-        self.fill and put_box(data_addr, line_len, bpp, x+wall_size, y+wall_size, self.width-2*wall_size, self.height-2*wall_size, GREEN) # inner
+        put_box(data_addr, line_len, bpp, x+wall_size, y+wall_size, self.width-2*wall_size, self.height-2*wall_size, BLACK) # inner
 
         self.west and put_box(data_addr, line_len, bpp, x, y, wall_size, self.height, BLUE) # west
         self.east and put_box(data_addr, line_len, bpp, x+self.width-wall_size, y, wall_size, self.height, BLUE) # east
@@ -96,27 +101,27 @@ def draw_pattern(cells: list, width: int, height: int):
     y = (height - 5)//2
 
     # 4
-    cells[x+0][y+0].fill = False
-    cells[x+0][y+1].fill = False
-    cells[x+0][y+2].fill = False
-    cells[x+1][y+2].fill = False
-    cells[x+2][y+2].fill = False
-    cells[x+2][y+2].fill = False
-    cells[x+2][y+3].fill = False
-    cells[x+2][y+4].fill = False
+    cells[x+0][y+0].pattern = True
+    cells[x+0][y+1].pattern = True
+    cells[x+0][y+2].pattern = True
+    cells[x+1][y+2].pattern = True
+    cells[x+2][y+2].pattern = True
+    cells[x+2][y+2].pattern = True
+    cells[x+2][y+3].pattern = True
+    cells[x+2][y+4].pattern = True
 
     # 2
-    cells[x+4][y+0].fill = False
-    cells[x+5][y+0].fill = False
-    cells[x+6][y+0].fill = False
-    cells[x+6][y+1].fill = False
-    cells[x+6][y+2].fill = False
-    cells[x+5][y+2].fill = False
-    cells[x+4][y+2].fill = False
-    cells[x+4][y+3].fill = False
-    cells[x+4][y+4].fill = False
-    cells[x+5][y+4].fill = False
-    cells[x+6][y+4].fill = False
+    cells[x+4][y+0].pattern = True
+    cells[x+5][y+0].pattern = True
+    cells[x+6][y+0].pattern = True
+    cells[x+6][y+1].pattern = True
+    cells[x+6][y+2].pattern = True
+    cells[x+5][y+2].pattern = True
+    cells[x+4][y+2].pattern = True
+    cells[x+4][y+3].pattern = True
+    cells[x+4][y+4].pattern = True
+    cells[x+5][y+4].pattern = True
+    cells[x+6][y+4].pattern = True
     
 
 def main():
@@ -130,10 +135,11 @@ def main():
     config.border_color = BLUE
     config.path_color = RED
     config.patter_color = GREEN
+    config.menu_color = GREEN
 
     m = mlx.Mlx()
     mlx_ptr = m.mlx_init()
-    win_ptr = m.mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "a-maze-ing")
+    win_ptr = m.mlx_new_window(mlx_ptr, WIDTH+20, HEIGHT+130, "a-maze-ing")
 
     img_ptr = m.mlx_new_image(mlx_ptr, WIDTH, HEIGHT)
     data_addr, bpp, line_len, endian = m.mlx_get_data_addr(img_ptr)
@@ -153,7 +159,14 @@ def main():
         for column in range(config.width):
             cells[row][column].render(data_addr, line_len, bpp)
 
-    m.mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0)
+    m.mlx_string_put(mlx_ptr, win_ptr, 10, HEIGHT+10+10, GREEN, "MENU")
+    m.mlx_string_put(mlx_ptr, win_ptr, 10, HEIGHT+10+30, GREEN, "1. Regenerate")
+    m.mlx_string_put(mlx_ptr, win_ptr, 10, HEIGHT+10+50, GREEN, "2. Show path")
+    m.mlx_string_put(mlx_ptr, win_ptr, 10, HEIGHT+10+70, GREEN, "3. Change color")
+    m.mlx_string_put(mlx_ptr, win_ptr, 10, HEIGHT+10+90, GREEN, "ESC. Quit")
+
+    m.mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 10, 10)
+
     m.mlx_key_hook(win_ptr, on_keypress, None)
     m.mlx_loop(mlx_ptr)
 
